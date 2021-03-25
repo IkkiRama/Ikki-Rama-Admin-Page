c6fc68ac5cd3b848a97c350239a080092de53acf
IkkiRama <georgeikkirama@gmail.com>
2021-03-26 06:47:08 +0700

    Menambahkan file pertama

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// light slider
+$(document).ready(function () {
+    $("#adaptive").lightSlider({
+        adaptiveHeight: true,
+        auto: true,
+        item: 1,
+        pauseOnHover: true,
+        slideMargin: 0,
+        loop: true,
+    });
+});
